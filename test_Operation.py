65cfce55f21ae8561b82e07f0e2f5f438634291c
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-03-13 15:26:59 +0530

    Refactor. removed unnecessary code

--- test_Operation.py
+++ test_Operation.py
@@ -9,10 +9,9 @@ class OperationTest(unittest.TestCase):
         self.addInputArrayOne = [1, 3, 3, 4]
 
     def test_add(self):
-        addarray = np.array(self.addInputArrayOne)
+        addarray = self.addInputArrayOne
         op = Operation(addarray)
         self.assertEqual(6, op.compute(addarray))
-
 
 if __name__ == '__main__':
     unittest.main()
